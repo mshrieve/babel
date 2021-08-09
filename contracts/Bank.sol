362ea70c6686d10872f7605e5d3953fbc33f610f
mike <mshrieve@gmail.com>
2021-08-08 23:10:01 -0700

    add bank, exp library

---
+++ contracts/Bank.sol
@@ -0,0 +1,78 @@
+// SPDX-License-Identifier: UNLICENSED
+
+pragma solidity ^0.8.0;
+
+import './Babel.sol';
+import './Libraries/Math.sol';
+
+// bonding curve contract
+//
+
+// bonding curve works  by taking time weighted volume
+// i.e. let q_i be the number of purchases in the i-th block
+// then cost function is C(q) = exp(a \sum q_i / n^i)
+// then each new block we mult by 1/n and exponentiate
+// need a 'floor' price, probably
+// don't need a max but a, and n will determine how quickly the curve goes up or down.
+
+contract Bank {
+    Babel immutable babel;
+
+    uint256 internal blockLastUpdated;
+    uint256 internal weight;
+    uint256 internal alpha = 152003093445049984; // -log_2(90%)
+    uint256 internal gamma = 1000000000000000; // .0001
+    uint256 internal floor = 100000000000000; // .001
+
+    event BabelMinted(address to, uint256 amount);
+
+    constructor(address _babel) {
+        babel = Babel(_babel);
+    }
+
+    function purchase(uint256 amount) external payable {
+        uint256 blocksSinceLastUpdated = block.number - blockLastUpdated;
+        // update Q if it has been more than one block since the last update
+        if (blocksSinceLastUpdated > 0) {
+            blockLastUpdated = block.number;
+            // if > 1684 we overflow the exponent
+            if (blocksSinceLastUpdated < 1684) {
+                weight =
+                    (weight * 10**18) /
+                    Math.exponentiate(alpha * blocksSinceLastUpdated);
+            }
+            // otherwise add nothing
+        }
+        // C(Q + q) - C(Q)
+        // path-independent within block
+        uint256 price = floor +
+            costFunction(weight + amount) -
+            costFunction(weight);
+        weight += amount;
+
+        // check for eth transfer
+        uint256 surplus = price - msg.value;
+        babel.mint(msg.sender, amount);
+        payable(msg.sender).transfer(surplus);
+    }
+
+    function getPrice(uint256 amount) external view returns (uint256 price) {
+        uint256 blocksSinceLastUpdated = block.number - blockLastUpdated;
+        uint256 weight_;
+        // update Q if it has been more than one block since the last update
+        if (blocksSinceLastUpdated > 0) {
+            // if > 1684 we overflow the exponent
+            if (blocksSinceLastUpdated < 1684)
+                weight_ =
+                    (weight * 10**18) /
+                    Math.exponentiate(alpha * blocksSinceLastUpdated);
+        } else weight_ = weight;
+        // C(Q + q) - C(Q)
+        // path-independent within block
+        price = floor + costFunction(weight_ + amount) - costFunction(weight_);
+    }
+
+    function costFunction(uint256 volume) internal view returns (uint256) {
+        return Math.exponentiate((gamma * volume) / 10**18);
+    }
+}
---
+++ tests/Math.ts
@@ -0,0 +1,110 @@
+import { ethers } from 'hardhat'
+import { Contract, Signer } from 'ethers'
+import BigNumber from 'bignumber.js'
+import Decimal from 'decimal.js'
+import { expect } from 'chai'
+Decimal.set({ precision: 27, rounding: 3 })
+BigNumber.config({ EXPONENTIAL_AT: 256 })
+
+describe('Math', () => {
+  let accounts: Signer[]
+  let Math: Contract
+
+  before(async () => {
+    const [owner] = await ethers.getSigners()
+    const MathFactory = await ethers.getContractFactory('MathHarness')
+    Math = await MathFactory.deploy()
+    await Math.deployed()
+  })
+
+  it('Should get powers of two', async () => {
+    const tableLength = 62
+    for (let i = 0; i < tableLength; i++) {
+      const actual = await Math.power_of_two_lookup(i)
+      const expected = new BigNumber(2).exponentiatedBy(i)
+      expect(actual.toString()).to.equal(expected.toString())
+    }
+  })
+
+  it('Should get the correct exponent from the exponent table', async () => {
+    const tableLength = 62
+    const eDecimals = new Decimal('1e18')
+    const fDecimals = new Decimal('1e-18')
+    const two = new Decimal(2)
+    for (let i = 0; i < tableLength; i++) {
+      const actual = await Math.exponent_table_lookup(i)
+      const expected = two
+        .pow(two.pow(i).mul(fDecimals))
+        .mul(eDecimals)
+        .floor()
+        .toString()
+      expect(actual.toString()).to.equal(expected.toString())
+    }
+  })
+
+  it('Should exponentiate integers properly', async () => {
+    const eDecimals = new BigNumber('1e18')
+    const two = new BigNumber(2)
+
+    // 10^18 < 2^60
+    // so, 10^18 * 2^196 < 2^256
+    for (let i = 0; i < 196; i++) {
+      const actual = await Math.exponentiate(eDecimals.times(i).toString())
+      const expected = two.pow(i).times(eDecimals)
+      expect(actual.toString()).to.equal(expected.toString())
+    }
+  })
+
+  it('Should exponentiate decimals properly', async () => {
+    const eDecimals = new Decimal('1e18')
+    const fDecimals = new Decimal('1e-18')
+    const two = new Decimal(2)
+    // we actually cannot expect the last few digits to be correct :(
+    const errorMargin = new Decimal('20')
+    for (let i = 0; i < 10; i++) {
+      const result = await Math.exponentiate(i)
+      const actual = new Decimal(result.toString()).div(errorMargin).floor()
+      const expected = two
+        .pow(fDecimals.times(i))
+        .times(eDecimals)
+        .div(errorMargin)
+        .floor()
+
+      expect(actual.toString()).to.equal(expected.toString())
+    }
+  })
+
+  it('Should pade exponentiate decimals properly', async () => {
+    const eDecimals = new Decimal('1e18')
+    const fDecimals = new Decimal('1e-18')
+    const two = new Decimal(2)
+    // we actually cannot expect the last few digits to be correct :(
+    const errorMargin = new Decimal('20')
+    for (let i = 0; i < 10; i++) {
+      const result = await Math.padeExponentiate(i)
+      const actual = new Decimal(result.toString()).div(errorMargin).floor()
+      const expected = two
+        .pow(fDecimals.times(i))
+        .times(eDecimals)
+        .div(errorMargin)
+        .floor()
+
+      expect(actual.toString()).to.equal(expected.toString())
+    }
+  })
+
+  it('Should find the logarithm of powers of two', async () => {
+    const eDecimals = new BigNumber('1e18')
+    const two = new BigNumber(2)
+
+    const errorMargin = new Decimal('20')
+    for (let i = 0; i < 196; i++) {
+      const actual = await Math.logarithm(
+        two.pow(i).times(eDecimals).toString()
+      )
+      const expected = eDecimals.times(i)
+
+      expect(actual.toString()).to.equal(expected.toString())
+    }
+  })
+})
